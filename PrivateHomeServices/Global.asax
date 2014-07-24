acda53b9ba3d6fc0038d15631e8ef4eb1bb37217
Augmensis <derek.hampton@hotmail.com>
2014-07-24 08:51:08 +0100

    Initial Commit

---
+++ PrivateHomeServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrivateHomeServices.MvcApplication" Language="C#" %>
---
+++ PrivateHomeServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrivateHomeServices.Startup))]
+namespace PrivateHomeServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
